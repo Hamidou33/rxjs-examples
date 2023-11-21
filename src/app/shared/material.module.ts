90ac1b5d68271f89ff1b0b7b04f027a85d2b4542
Hamidou33 <hamid.benallel.scicog@gmail.com>
2023-11-21 18:33:41 +0100

    feat(): début lazyloading des composants (creation) enfants et standalone du composant home

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
